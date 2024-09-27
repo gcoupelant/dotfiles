a3dd8a28dd5df337141438cadcf574eaa6bd75ff
Guillaume Coupelant <guillaume.coupelant@ubisoft.com>
2024-09-27 11:12:20 +0800

    Add support for 1Password CLI in WSL

--- wsl/.zsh_custom/wsl.zsh
+++ wsl/.zsh_custom/wsl.zsh
@@ -5,3 +5,8 @@ export PATH="/mnt/c/Windows/System32/:$PATH"
 export PATH="/mnt/c/Users/$USER/AppData/Local/Programs/Microsoft VS Code/bin:$PATH"
 # User bin
 export PATH="/mnt/c/Users/$USER/bin:$PATH"
+
+# 1Password CLI
+if [ ! -z $(find /mnt/c/Users/$USER/AppData/Local/Microsoft/WinGet/Packages -name op.exe) ]; then
+  alias op="$(find /mnt/c/Users/$USER/AppData/Local/Microsoft/WinGet/Packages -name op.exe)"
+fi
